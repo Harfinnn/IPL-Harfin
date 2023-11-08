78e1a31de3795926186d9fdce63f055dea6cd7c4
Harfin <harfinaqbil07@gmail.com>
2023-11-08 10:45:23 +0700

    perubahan customer

--- CleanCode/Customer.java
+++ CleanCode/Customer.java
@@ -1,11 +1,7 @@
 package CleanCode;
 
-public class Customer {
-    class DtaRcrd102 { 
-        private Date genymdhms; 
-        private Date modymdhms; 
-        private final String pszqint = "102"; 
-        
-       } 
-       
+class Customer {
+    private Date generationTimestamp; 
+    private Date modificationTimestamp; 
+    private final String recordId = "102"; 
 }
