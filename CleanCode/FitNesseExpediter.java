d7dfae046082f117ed35689d511922e7ff11d110
Harfin <harfinaqbil07@gmail.com>
2023-11-08 11:23:11 +0700

    Good Formatting

--- CleanCode/FitNesseExpediter.java
+++ CleanCode/FitNesseExpediter.java
@@ -1,16 +1,15 @@
 package CleanCode;
 
 public class FitNesseExpediter implements ResposeSender {
-    private Socket socket;
-    private InputStream input;
-    private OutputStream output;
-    private Request request;
-    private Response response;
-    private FitNesseContext context;
-    protected long requestParsingTimeLimit;
-    private long requestProgress;
-    private long 
-    requestParsingDeadLine;
-    private boolean hasError;
+    private     Socket          socket;
+    private     InputStream     input;
+    private     OutputStream    output;
+    private     Request         request;
+    private     Response        response;
+    private     FitNesseContext context;
+    protected   long            requestParsingTimeLimit;
+    private     long            requestProgress;
+    private     long            requestParsingDeadLine;
+    private     boolean         hasError;
 
 }
