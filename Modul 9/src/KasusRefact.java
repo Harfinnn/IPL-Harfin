e4da0dcc281b75c443fcbce4a8d174af088d8b75
Harfin <harfinaqbil07@gmail.com>
2023-12-20 11:33:03 +0700

    KasusRefact(sesudah Refactor)

--- Modul 9/src/KasusRefact.java
+++ Modul 9/src/KasusRefact.java
@@ -8,70 +8,59 @@
  * @author Harfin aqbil falah
  */
 public class KasusRefact {
-    private boolean ValidateRequest( CustomerInquiryRequest request, int 
-customerIDFieldLength, int productFieldLength ){
- if( request.CustomerProduct.ProductNumber != null && 
-request.Customer.CustomerID != null )
- {
- if( request.CustomerProduct.ProductNumber != string.Empty && 
-request.Customer.CustomerID != string.Empty ){
- // Both were populated
- throw new BusinessException( 
- HandledErrors.InvalidBothParameterMessage );
- }
- else if( request.Customer.CustomerID == string.Empty && 
-request.CustomerProduct.ProductNumber == string.Empty ) {
- // if objects were instantiated but not populated
- throw new BusinessException( 
- HandledErrors.CustomerEmptyMessage );
- }
- else if( request.Customer.CustomerID != "" ){
- // Note: ProductNumber was equal
- // to string.empty Check Customer ID length
- if( request.Customer.CustomerID.Length > 
-customerIDFieldLength ){
- throw new BusinessException( 
- HandledErrors.CustomerInvalidLengthMessage );
- }
- }else{
- // Note: CustomerID was equal
- // to string.empty check Product Length
- if( request.CustomerProduct.ProductNumber.Length > 
-productFieldLength ){
- throw new BusinessException(
- HandledErrors.ProductInvalidLengthMessage );
- }
- }
- }else if( request.CustomerProduct.ProductNumber == null && 
-request.Customer.CustomerID == null ){
- // Both were null
- throw new BusinessException( HandledErrors.CustomerEmptyMessage );
- }else if( request.CustomerProduct.ProductNumber == null ){
- // ProductNumber was null and CustomerID was not null
- if( request.Customer.CustomerID.Length > customerIDFieldLength ){
- throw new BusinessException(
- HandledErrors.CustomerInvalidLengthMessage );
- }
- }else{ // ProductNumber not null and CustomerID was null
- // Check Product Length
- if( request.CustomerProduct.ProductNumber.Length > 
-productFieldLength ) {
- throw new BusinessException(
- HandledErrors.ProductInvalidLengthMessage );
- }
- }
- // Set objects below with formatted data i.e PadLeft
- if( request.Customer.CustomerID != null ){
- request.Customer.CustomerID = request.Customer.CustomerID.PadLeft(
-customerIDFieldLength, 
- Convert.ToChar( "0", CultureInfo.CurrentCulture ) );
- }
- if( request.CustomerProduct.ProductNumber != null ){
- request.CustomerProduct.ProductNumber =
- request.CustomerProduct.ProductNumber.PadLeft(
- productFieldLength, 
- Convert.ToChar( "0", CultureInfo.CurrentCulture ) );
- }
- return true;
+    private void ValidateRequest(CustomerInquiryRequest request, int customerFieldLength, int productFieldLength){
+    // 1. Check both parameters are not null and not empty strings
+    CheckCustomerInquiryNotNullOrEmpty( request );
+    // 2. Check both parameters aren't populated
+    CheckCustomerInquiryNullOrEmpty( request );
+    // 3. Check CustomerID for field length and pad the parameter
+    CheckCustomerIDValid( request, customerFieldLength );
+    // 4. Check ProductNumber for field length and pad the parameter
+    CheckProductNumberValid( request, productFieldLength );
+    }
+    private void CheckCustomerInquiryNotNullOrEmpty(CustomerInquiryRequest request)
+    {
+    // Check both parameters are not null or empty string
+    if( !string.IsNullOrEmpty( request.CustomerProduct.ProductNumber ) &&
+        !string.IsNullOrEmpty( request.Customer.CustomerID ) )
+    {
+        // Both were populated
+        throw new BusinessException(
+            HandledErrors.InvalidBothParameterMessage );
+    }
 }
+private void CheckCustomerInquiryNullOrEmpty(CustomerInquiryRequest request ){
+    if( string.IsNullOrEmpty( request.Customer.CustomerID ) && string.IsNullOrEmpty( request.CustomerProduct.ProductNumber ) ){
+    // Both are null or empty string
+    throw new BusinessException(
+        HandledErrors.CustomerEmptyMessage );
+    }
+}
+private void CheckCustomerIDValid( CustomerInquiryRequest request, int customerIDFieldLength ){ 
+    if( string.IsNullOrEmpty( request.Customer.CustomerID ) ){ 
+    // Check Customer ID length
+    if( request.Customer.CustomerID.Length > customerIDFieldLength ){
+        throw new BusinessException(
+            HandledErrors.CustomerInvalidLengthMessage );
+        }
+    // Pad the left of the customer id
+    request.Customer.CustomerID = request.Customer.CustomerID.PadLeft(customerIDFieldLength,Convert.ToChar( "0", CultureInfo.CurrentCulture ) );
+    }
+}
+private void CheckProductNumberValid( CustomerInquiryRequest request, int productFieldLength )
+    { 
+        if( string.IsNullOrEmpty( request.CustomerProduct.ProductNumber ) )
+    {     
+        // Check Product Length
+        if( request.CustomerProduct.ProductNumber.Length > productFieldLength )
+    {
+        throw new BusinessException( 
+            HandledErrors.ProductInvalidLengthMessage );
+    }
+        // Pad the left of the product number 
+        request.CustomerProduct.ProductNumber = 
+            request.CustomerProduct.ProductNumber.PadLeft(
+            productFieldLength, 
+            Convert.ToChar( "0", CultureInfo.CurrentCulture ) );
+    }
 }
