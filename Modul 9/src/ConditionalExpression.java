62b314359a5827cc5294940cb64a8af0cd6d27f0
Harfin <harfinaqbil07@gmail.com>
2023-12-20 11:12:12 +0700

    Menyederhanakan Kondisi If (Conditional Expression)

---
+++ Modul 9/src/ConditionalExpression.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Harfin aqbil falah
+ */
+public class ConditionalExpression {
+if (score > 700)
+    accept();
+else if ((income >= 40000) && (income <= 100000)
+        && authorized && (score > 500))
+    accept();
+else if (income > 100000)
+    accept();
+else
+    reject();
+}
