355677390d3d312b42cf1c36c69419c86e84921a
Harfin <harfinaqbil07@gmail.com>
2023-12-20 11:20:04 +0700

    contoh kasus(sebelum refactoring)

---
+++ Modul 9/src/KasusRefact.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Harfin aqbil falah
+ */
+public class KasusRefact {
+    private boolean ValidateRequest( CustomerInquiryRequest request, int 
+customerIDFieldLength, int productFieldLength ){
+ if( request.CustomerProduct.ProductNumber != null && 
+request.Customer.CustomerID != null )
+ {
+ if( request.CustomerProduct.ProductNumber != string.Empty && 
+request.Customer.CustomerID != string.Empty ){
+ // Both were populated
+ throw new BusinessException( 
+ HandledErrors.InvalidBothParameterMessage );
+ }
+ else if( request.Customer.CustomerID == string.Empty && 
+request.CustomerProduct.ProductNumber == string.Empty ) {
+ // if objects were instantiated but not populated
+ throw new BusinessException( 
+ HandledErrors.CustomerEmptyMessage );
+ }
+ else if( request.Customer.CustomerID != "" ){
+ // Note: ProductNumber was equal
+ // to string.empty Check Customer ID length
+ if( request.Customer.CustomerID.Length > 
+customerIDFieldLength ){
+ throw new BusinessException( 
+ HandledErrors.CustomerInvalidLengthMessage );
+ }
+ }else{
+ // Note: CustomerID was equal
+ // to string.empty check Product Length
+ if( request.CustomerProduct.ProductNumber.Length > 
+productFieldLength ){
+ throw new BusinessException(
+ HandledErrors.ProductInvalidLengthMessage );
+ }
+ }
+ }else if( request.CustomerProduct.ProductNumber == null && 
+request.Customer.CustomerID == null ){
+ // Both were null
+ throw new BusinessException( HandledErrors.CustomerEmptyMessage );
+ }else if( request.CustomerProduct.ProductNumber == null ){
+ // ProductNumber was null and CustomerID was not null
+ if( request.Customer.CustomerID.Length > customerIDFieldLength ){
+ throw new BusinessException(
+ HandledErrors.CustomerInvalidLengthMessage );
+ }
+ }else{ // ProductNumber not null and CustomerID was null
+ // Check Product Length
+ if( request.CustomerProduct.ProductNumber.Length > 
+productFieldLength ) {
+ throw new BusinessException(
+ HandledErrors.ProductInvalidLengthMessage );
+ }
+ }
+ // Set objects below with formatted data i.e PadLeft
+ if( request.Customer.CustomerID != null ){
+ request.Customer.CustomerID = request.Customer.CustomerID.PadLeft(
+customerIDFieldLength, 
+ Convert.ToChar( "0", CultureInfo.CurrentCulture ) );
+ }
+ if( request.CustomerProduct.ProductNumber != null ){
+ request.CustomerProduct.ProductNumber =
+ request.CustomerProduct.ProductNumber.PadLeft(
+ productFieldLength, 
+ Convert.ToChar( "0", CultureInfo.CurrentCulture ) );
+ }
+ return true;
+}
+}
