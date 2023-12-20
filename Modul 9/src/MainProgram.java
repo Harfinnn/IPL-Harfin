8575d87573c5bb41c7cc72f19d9db70c5800274a
Harfin <harfinaqbil07@gmail.com>
2023-12-20 10:58:26 +0700

    Sebelum Refactoring

---
+++ Modul 9/src/MainProgram.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Harfin aqbil falah
+ */
+public class MainProgram {
+    public static void main(String[]args){
+    Person person;
+    person.name = "Bob Smith";
+    System.out.print = person.name;
+    }
+}
