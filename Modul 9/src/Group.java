ff048cfbc03655f7c0a10229bb25f714b731dc65
Harfin <harfinaqbil07@gmail.com>
2023-12-23 06:54:50 +0700

    Sebelum Refactoring

---
+++ Modul 9/src/Group.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Harfin aqbil falah
+ */
+public class Group {
+    // Gets users sorted by the most recently registered user
+    public List getUsers() {
+        List users = new ArrayList();
+        if (!new File(persistencePath()).exists())
+            return users;
+        File[] files = new File(persistencePath()).listFiles(); 
+        for (File file : files)
+            if (file.isDirectory())
+                users.add(new User(file.getName(), this));
+        Collections.sort(users, new User.UserComparatorByDescendingRegistration());
+        return users;
+    }
